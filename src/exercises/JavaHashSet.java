138dcfe190b6491ee719e7fe4c529cdd10531047
Jacek Dlugopolski <34134192+JacekDl@users.noreply.github.com>
2019-11-04 07:48:05 +0100

    added solution to Java HashSet problem

---
+++ src/exercises/JavaHashSet.java
@@ -0,0 +1,35 @@
+package exercises;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+
+//https://www.hackerrank.com/challenges/java-hashset/problem
+
+public class JavaHashSet {
+
+public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int t = s.nextInt();
+        String[] pair_left = new String[t];
+        String[] pair_right = new String[t];
+        
+        for (int i = 0; i < t; i++) {
+            pair_left[i] = s.next();
+            pair_right[i] = s.next();
+        }
+
+//Write your code here
+        HashSet<String> mySet = new HashSet<String>();	
+        for(int i = 0; i < t; i++) {
+        	String name = pair_left[i] + " " + pair_right[i];	//concatenate strings
+        	mySet.add(name);	//add newly created string to hashset
+        	System.out.println(mySet.size());	//print no. of elements currently hold in hashset
+        }
+        
+        
+   }
+}
