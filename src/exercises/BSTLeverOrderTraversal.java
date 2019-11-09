4cc535644470394975ea689799bf63d77ce0027e
Jacek Dlugopolski <34134192+JacekDl@users.noreply.github.com>
2019-11-09 19:52:51 +0100

    Moved Node class to separate file.
    Added comment.
    Changed indentation.

--- src/exercises/BSTLeverOrderTraversal.java
+++ src/exercises/BSTLeverOrderTraversal.java
@@ -3,31 +3,23 @@ package exercises;
 import java.util.*;
 import java.io.*;
 
-//TODO Jacek - I would rather put it in a separate file/package, or make this class private/static
-class Node{
-    Node left,right;
-    int data;
-    Node(int data){
-        this.data=data;
-        left=right=null;
-    }
-}
+//TODO Jacek - I would rather put it in a separate file/package, or make this class private/static 
+//--> DONE (separate file created)
 
 //https://www.hackerrank.com/challenges/30-binary-trees/problem
 class BSTLevelOrderTraversal{
-
-static void levelOrder(Node root){
-	ArrayDeque<Node> queue = new ArrayDeque<>();
-	if(root != null) {
-		queue.add(root);
+	static void levelOrder(Node root){
+		ArrayDeque<Node> queue = new ArrayDeque<>();
+		if(root != null) {
+			queue.add(root);
+		}
+		while(!queue.isEmpty()) {
+			Node current = queue.removeFirst();
+			System.out.print(current.data + " ");
+			if(current.left != null) queue.add(current.left);
+			if(current.right != null) queue.add(current.right);
+		}
 	}
-	while(!queue.isEmpty()) {
-		Node current = queue.removeFirst();
-		System.out.print(current.data + " ");
-		if(current.left != null) queue.add(current.left);
-		if(current.right != null) queue.add(current.right);
-	}
-}
 
 	private static Node insert(Node root,int data){
         if(root == null){
@@ -46,6 +38,7 @@ static void levelOrder(Node root){
             return root;
         }
     }
+	
     public static void main(String args[]){
             Node root = null;
             root = insert(root, 5);
@@ -54,5 +47,5 @@ static void levelOrder(Node root){
             root = insert(root, 4);
      
             levelOrder(root);
-        }	
+    }	
 }
