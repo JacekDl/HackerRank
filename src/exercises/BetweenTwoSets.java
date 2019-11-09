a0dfa2db597284d3b49e2e7aa75eb1a8fdc25ad6
Jacek Dlugopolski <34134192+JacekDl@users.noreply.github.com>
2019-11-09 20:21:17 +0100

    refactored code of BetweenTwoSets class

--- src/exercises/BetweenTwoSets.java
+++ src/exercises/BetweenTwoSets.java
@@ -7,29 +7,38 @@ import java.util.List;
 //	class name = name of problem
 public class BetweenTwoSets {
 
-	//TODO Jacek naming conventions - at least l1, l2, or list1, list2. a or a tells you nothing.
-	private static int getTotal(List<Integer> a, List<Integer> b) {
+	//TODO Jacek naming conventions - at least l1, l2, or list1, list2. a or a tells you nothing. --> DONE
+	private static int getTotal(List<Integer> listA, List<Integer> listB) {
 		int answer = 0;
-		
-		int aMax = 0;
+		int listAMax = 0;
 		
 		//TODO Jacek - in Java please always use {...} after if/else/for/... statements, even if there is only one line. Same for all files/occurrences in repository.
 		
 		//TODO Jacek - what will happen (conversion Integer -> int), if a or b contains null and you iterate like for(int i : a)? (not the case in this exercise, but general note)
-		for(int i : a) if(i > aMax) aMax = i;
+		for(int i : listA) {
+			if(i > listAMax) {
+				listAMax = i;
+			}
+		}
 			
-		int bMin = 100;
-		for(int i : b) if(i < bMin) bMin = i;
+		int listBMin = 100;
+		for(int i : listB) {
+			if(i < listBMin) {
+				listBMin = i;
+			}
+		}
 		
-		for(int i = aMax; i <= bMin; i++) {
+		for(int i = listAMax; i <= listBMin; i++) {
 			boolean isFactor = true;
-			for(int j : a) {
+			for(int j : listA) {
 				if(i % j != 0) isFactor = false;
 			}
-			for(int k : b) {
+			for(int k : listB) {
 				if(k % i != 0) isFactor = false;
 			}
-			if(isFactor) answer++;
+			if(isFactor) {
+				answer++;
+			}
 		}
 		return answer;
 	} 
@@ -39,14 +48,14 @@ public class BetweenTwoSets {
 		//TODO Jacek - another useful library to use: Google Guava
 		// using it, you can e.g. initialize list in singe line like
 		// Lists<Integer> = Lists.newArrayList(3,4) - add .jar or like in Maven/Gradle project
-		List<Integer> a = new ArrayList<Integer>();
-		a.add(3);
-		a.add(4);
-		List<Integer> b = new ArrayList<Integer>();
-		b.add(24);
-		b.add(48);
+		List<Integer> listA = new ArrayList<Integer>();
+		listA.add(3);
+		listA.add(4);
+		List<Integer> listB = new ArrayList<Integer>();
+		listB.add(24);
+		listB.add(48);
 		
-		int total = getTotal(a, b);
+		int total = getTotal(listA, listB);
 		System.out.println(total);
 		
 	}
