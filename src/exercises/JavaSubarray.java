b11003ff0e0ca3f9de90f2624f36d1e5c7eb3c83
Jacek Dlugopolski <34134192+JacekDl@users.noreply.github.com>
2019-11-12 10:15:51 +0100

    changed names of variables

--- src/exercises/JavaSubarray.java
+++ src/exercises/JavaSubarray.java
@@ -6,10 +6,10 @@ import java.util.Scanner;
 //https://www.hackerrank.com/challenges/java-negative-subarray/problem?h_r=internal-search
 public class JavaSubarray {
 
-	private static int sumSubarray(int[] arr, int i, int j) {
+	private static int sumSubarray(int[] arr, int startIndex, int endIndex) {
 		int sum = 0;
-		for(int s = i; s <= j; s++) {
-			sum += arr[s];
+		for(int i = startIndex; i <= endIndex; i++) {
+			sum += arr[i];
 		}
 		return sum;
 	}
@@ -22,7 +22,9 @@ public class JavaSubarray {
     	for(int i = 0; i < n; i++) {
     		for(int j = 0; j < n; j++) {
     			int answer = sumSubarray(arr, i, j);
-    			if(answer < 0) numberOfNegativeSubarrays++;
+    			if(answer < 0) {
+    				numberOfNegativeSubarrays++;
+    			}
     		}
     	}
     	System.out.println(numberOfNegativeSubarrays);
