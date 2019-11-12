0982686e995c838ed664313e62fbabb527e99e43
Jacek Dlugopolski <34134192+JacekDl@users.noreply.github.com>
2019-11-12 08:32:33 +0100

    changed ArrayList to List interface

--- src/exercises/JavaArrayList.java
+++ src/exercises/JavaArrayList.java
@@ -12,15 +12,16 @@ public class JavaArrayList {
 		int arrayLength = sc.nextInt();
 		
 		//TODO Jacek always initialize with interface, whenever possible
-		//List<List<Integer>> mrArray = ...
-		ArrayList<ArrayList<Integer>> myArray = new ArrayList<ArrayList<Integer>>();
+		//List<List<Integer>> mrArray = ... --> OK I have tried to do this in this way, but I get message: Cannot convert form List to Arraylist
+		//--> OK2 Now it is done properly
+		List<List<Integer>> myArray = new ArrayList<List<Integer>>();
 		for(int i = 0; i < arrayLength; i++) {
-			ArrayList<Integer> subArray = new ArrayList<Integer>();
+			List<Integer> subArray = new ArrayList<Integer>();
 			myArray.add(subArray);
 		}
 		
 		for(int i = 0; i < arrayLength; i++) {
-			ArrayList<Integer> subArray = myArray.get(i);
+			List<Integer> subArray = myArray.get(i);
 			int subArrayLenght = sc.nextInt();
 			
 			for(int j = 0; j < subArrayLenght; j++) {
@@ -37,7 +38,7 @@ public class JavaArrayList {
 			y = sc.nextInt() - 1;
 			
 			try {
-				ArrayList<Integer> subArray = myArray.get(x);
+				List<Integer> subArray = myArray.get(x);
 				Integer answer = subArray.get(y);
 				System.out.println(answer);
 			}
