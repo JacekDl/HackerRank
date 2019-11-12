3ec18c847d1de1a3d295c5f58d377050e94cc536
Jacek Dlugopolski <34134192+JacekDl@users.noreply.github.com>
2019-11-12 09:48:58 +0100

    changed HashMap to Map interface
    created field Scanner

--- src/exercises/JavaMap.java
+++ src/exercises/JavaMap.java
@@ -1,29 +1,35 @@
 package exercises;
 
 
-import java.util.*;
-import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
 //https://www.hackerrank.com/challenges/phone-book/problem
 class JavaMap{
+	private static Scanner in = new Scanner(System.in);
+
 	public static void main(String []argh)
 	{
-		HashMap<String, Integer> phoneBook = new HashMap<>();
-		Scanner in = new Scanner(System.in);
-		int n=in.nextInt();
+		Map<String, Integer> phoneBook = new HashMap<>();
+		int n = in .nextInt();
 		in.nextLine();
 		for(int i=0;i<n;i++)
 		{
-			String name=in.nextLine();
-			int phone=in.nextInt();
-			in.nextLine();
+			String name=in .nextLine();
+			int phone=in .nextInt();
+			in .nextLine();
 			phoneBook.put(name, phone);
 		}
 		while(in.hasNext())
 		{
-			String s=in.nextLine();
-			if(phoneBook.containsKey(s)) System.out.println(s + "=" +phoneBook.get(s));
-			else System.out.println("Not found");
+			String s=in .nextLine();
+			if(phoneBook.containsKey(s)) {
+				System.out.println(s + "=" +phoneBook.get(s));
+			}
+			else {
+				System.out.println("Not found");
+			}
 		}
 	}
 }
