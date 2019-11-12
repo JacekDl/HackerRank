e9976f484a0f3e928646aa2ae7ae8a5e0cc5cae8
Jacek Dlugopolski <34134192+JacekDl@users.noreply.github.com>
2019-11-12 09:27:06 +0100

    changed HashSet to Set in JavaHashSet class

--- src/exercises/JavaHashSet.java
+++ src/exercises/JavaHashSet.java
@@ -1,33 +1,32 @@
 package exercises;
 
-import java.io.*;
-import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
 
 //https://www.hackerrank.com/challenges/java-hashset/problem
 public class JavaHashSet {
 
 	public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        int t = s.nextInt();
-        String[] pair_left = new String[t];
-        String[] pair_right = new String[t];
-        
-        for (int i = 0; i < t; i++) {
-            pair_left[i] = s.next();
-            pair_right[i] = s.next();
-        }
+		Scanner scan = new Scanner(System.in);
+		int numberOfPairs = scan.nextInt();
+		String[] pair_left = new String[numberOfPairs];
+		String[] pair_right = new String[numberOfPairs];
 
-        //TODO Jacek initialize with interface
-        HashSet<String> mySet = new HashSet<String>();	
-        for(int i = 0; i < t; i++) {
-        	String name = pair_left[i] + " " + pair_right[i];		//concatenate strings
-        	mySet.add(name);										//add newly created string to hashset
-        	System.out.println(mySet.size());						//print no. of elements currently hold in hashset
-        }
-        
-        
-   }
+		for (int i = 0; i < numberOfPairs; i++) {
+			pair_left[i] = scan.next();
+			pair_right[i] = scan.next();
+		}
+
+		scan.close();
+		//TODO Jacek initialize with interface --> DONE Hashset changed to Set
+		Set<String> mySet = new HashSet<String>();	
+		for(int i = 0; i < numberOfPairs; i++) {
+			String name = pair_left[i] + " " + pair_right[i];		//concatenate strings
+			mySet.add(name);										//add newly created string to hashset, 
+			System.out.println(mySet.size());						//print no. of elements currently hold in hashset
+		}
+
+
+	}
 }
