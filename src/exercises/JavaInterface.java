28802d5606184def74fd206e04afd66ba24ccc8f
Jacek Dlugopolski <34134192+JacekDl@users.noreply.github.com>
2019-11-12 09:38:31 +0100

    removed MyCalculator class and  AdvancedAritmetic interface from file

--- src/exercises/JavaInterface.java
+++ src/exercises/JavaInterface.java
@@ -1,34 +1,21 @@
 package exercises;
 
 import java.util.*;
-interface AdvancedArithmetic{
-  int divisor_sum(int n);
-}
-
-class MyCalculator implements AdvancedArithmetic{
-    public int divisor_sum(int n){
-        int answer = 0;
-        for(int i = 1; i <= n; i++) {
-        	if(n % i == 0) answer += i;
-        }
-    	return answer;
-    }
-}
 
 //https://www.hackerrank.com/challenges/java-interface/problem
 class JavaInterface{
     public static void main(String []args){
         MyCalculator my_calculator = new MyCalculator();
         System.out.print("I implemented: ");
-        ImplementedInterfaceNames(my_calculator);
+        implementedInterfaceNames(my_calculator);
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         System.out.print(my_calculator.divisor_sum(n) + "\n");
       	sc.close();
     }
     
-    //TODO Jacek - Java naming conventions
-    static void ImplementedInterfaceNames(Object o){
+    //TODO Jacek - Java naming conventions --> name of method starts with small letter
+    static void implementedInterfaceNames(Object o){
         Class[] theInterfaces = o.getClass().getInterfaces();
         for (int i = 0; i < theInterfaces.length; i++){
             String interfaceName = theInterfaces[i].getName();
