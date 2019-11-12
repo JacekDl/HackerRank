7612f3c5764b9b614515ad5e2391e978c7ca6b12
Jacek Dlugopolski <34134192+JacekDl@users.noreply.github.com>
2019-11-12 10:29:30 +0100

    removed Calculator class from this file

--- src/exercises/MoreExceptions.java
+++ src/exercises/MoreExceptions.java
@@ -1,16 +1,5 @@
 package exercises;
 
-class Calculator{
-	
-	int power(int n, int p) throws Exception{ 
-		if(n < 0 || p < 0) {
-			throw new Exception("n and p should be non-negative");
-		}
-		return (int)Math.pow(n, p);
-	}
-	
-}
-
 //https://www.hackerrank.com/challenges/30-more-exceptions/problem
 public class MoreExceptions {
 	
