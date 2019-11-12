b68458cc2acf2b6e139f7e21efb07c8191dfb39a
Jacek Dlugopolski <34134192+JacekDl@users.noreply.github.com>
2019-11-12 09:20:41 +0100

    refactored code of JavaGenerics class

--- src/exercises/JavaGenerics.java
+++ src/exercises/JavaGenerics.java
@@ -4,34 +4,32 @@ package exercises;
 import java.io.IOException;
 import java.lang.reflect.Method;
 
-class ArrayPrinter{
-    static <T> void printArray(T[] array){
-        for(T element : array){
-            System.out.println(element);
-        }
-    }
-}
+
 
 //https://www.hackerrank.com/challenges/java-generics/problem
 public class JavaGenerics {
     public static void main(String args[]) {
-        ArrayPrinter myPrinter = new ArrayPrinter();
+        //ArrayPrinter myPrinter = new ArrayPrinter();
         Integer[] intArray = { 1, 2, 3 };
         String[] stringArray = {"Hello", "World"};
         
-        //TODO Jacek - printArray(...) should be invoked like static method
-        myPrinter.printArray(intArray);
-        myPrinter.printArray(stringArray);
+        //TODO Jacek - printArray(...) should be invoked like static method --> DONE: static methods does not require class instance
+        ArrayPrinter.printArray(intArray);
+        ArrayPrinter.printArray(stringArray);
         int count = 0;
 
         for (Method method : ArrayPrinter.class.getDeclaredMethods()) {
             String name = method.getName();
 
-            if(name.equals("printArray"))
-                count++;
+            if(name.equals("printArray")) {
+            	count++;
+            }
+                
         }
 
-        if(count > 1)System.out.println("Method overloading is not allowed!");
+        if(count > 1) {
+        	System.out.println("Method overloading is not allowed!");
+        }
       
     }
 }
