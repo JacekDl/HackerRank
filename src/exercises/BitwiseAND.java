30ebe9e94dd9e817e262efc1c521b00186f40173
Jacek Dlugopolski <34134192+JacekDl@users.noreply.github.com>
2019-11-12 08:04:02 +0100

    minor changes to BitwiseAND class

--- src/exercises/BitwiseAND.java
+++ src/exercises/BitwiseAND.java
@@ -5,23 +5,22 @@ public class BitwiseAND {
 
 	public static void main(String[] args) {
 		
-		int n = 2;
-		int k = 2;
+		int sizeOfSet = 2;
+		int givenInteger = 2;
 		int numberToInsert = 1;
-		int[] s = new int[n];
+		int[] mySet = new int[sizeOfSet];
 		
-		for(int i = 0; i < n; i++) {
-			s[i] = numberToInsert++;	
+		for(int i = 0; i < sizeOfSet; i++) {
+			mySet[i] = numberToInsert++;	
 		}
 		
 		int max = 0;
-		for(int i = 0; i < n-1; i++) {
-			for(int j = i + 1; j < n; j++) {
-				int calc = s[i] & s[j];
-				if(calc > max && calc < k) max = calc;
+		for(int i = 0; i < sizeOfSet-1; i++) {
+			for(int j = i + 1; j < sizeOfSet; j++) {
+				int calc = mySet[i] & mySet[j];
+				if(calc > max && calc < givenInteger) max = calc;
 			}
 		}
 		System.out.println(max);
 	}
-
 }
