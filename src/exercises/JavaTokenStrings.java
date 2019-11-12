1b863821724a385ac7364607c7706c8c6ff11277
Jacek Dlugopolski <34134192+JacekDl@users.noreply.github.com>
2019-11-12 10:25:12 +0100

    refactored code of JavaTokenStings class

--- src/exercises/JavaTokenStrings.java
+++ src/exercises/JavaTokenStrings.java
@@ -1,20 +1,18 @@
 package exercises;
 
-import java.io.*;
-import java.util.*;
-
 //https://www.hackerrank.com/challenges/java-string-tokens/problem
 public class JavaTokenStrings {
 
 	    public static void main(String[] args) {
 	        
-	       String s = "                ";
+	       String s = "            ";
 	       s = s.concat("!");
-	       String d = s + "!";
  	       String[] splitString = s.trim().split("[ !,?._'@]+");
  	       
  	       System.out.println(splitString.length);
- 	       for(String i : splitString) System.out.println(i);
+ 	       for(String i : splitString) {
+ 	    	   System.out.println(i);
+ 	       }
 	    }
 	}
 
