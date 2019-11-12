cb306bc4f2fe050ef80003e947ed9ffffc0e6fcb
Jacek Dlugopolski <34134192+JacekDl@users.noreply.github.com>
2019-11-12 10:21:06 +0100

    renamed variables

--- src/exercises/JavaSubstringComparison.java
+++ src/exercises/JavaSubstringComparison.java
@@ -4,27 +4,31 @@ package exercises;
 public class JavaSubstringComparison {
 
 	public static void main(String[] args) {
-		String s = "welcometojava";
-		int k = 3;
+		String stringToCheck = "welcometojava";
+		int lengthOfSubstring = 3;
 		
-		int begin = s.length() - k;
+		int begin = stringToCheck.length() - lengthOfSubstring;
 		System.out.println("begin = " + begin);
 		
-		String min = s.substring(begin);
+		String min = stringToCheck.substring(begin);
 		String max = min;
 		System.out.println(min + " " + max);
 		
 		begin--;
-		int end = s.length() - 1;
+		int end = stringToCheck.length() - 1;
 		System.out.println("end = " + end);
 		
 		for(int i = begin; i >= 0; i--) {
-			String current = s.substring(i, end);
+			String current = stringToCheck.substring(i, end);
 			System.out.println(current);
 			int ansMin = current.compareTo(min);
-			if(ansMin < 0) min = current;
+			if(ansMin < 0) {
+				min = current;
+			}
 			int ansMax = current.compareTo(max);
-			if(ansMax > 0) max = current;
+			if(ansMax > 0) {
+				max = current;
+			}
 			end--;
 		}
 		
